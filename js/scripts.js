697473451cce8ebe72c1713bd558e7e5af87dc98
Krissy-Arci <109753140+Krissy-Arci@users.noreply.github.com>
2022-08-30 17:27:45 -0400

    Update js/scripts.js
    Fixed errors flagged by linter-js-standard

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,10 @@
-console.log("Welcome to my portfolio!😸👋🏼\n*This is the latest version of my self-coded website.\nFor my main website, please visit arcidiaconodesign.com")
+console.log('Welcome to my portfolio!😸👋🏼\n*This is the latest version of my self-coded website.\nFor my main website, please visit arcidiaconodesign.com')
 
-
-function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
   if (x.className === 'navtoggle') {
-    x.className += ' responsive';
+    x.className += ' responsive'
   } else {
-    x.className = 'navtoggle';
+    x.className = 'navtoggle'
   }
 }
